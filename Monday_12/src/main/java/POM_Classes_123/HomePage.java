bd1f93586d380fce17515155faa831943f3d4dfa
sachin <sachinkolhe1551@gmail.com>
2023-06-18 19:14:26 +0530

    abcde123

--- Monday_12/src/main/java/POM_Classes_123/HomePage.java
+++ Monday_12/src/main/java/POM_Classes_123/HomePage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
+	
 	@FindBy(xpath="(//summary[@class='Header-link'])[2]")private WebElement accountbtn;
 	@FindBy(xpath="//strong[text()='sachin7173']")private WebElement verifyusername;
 	
